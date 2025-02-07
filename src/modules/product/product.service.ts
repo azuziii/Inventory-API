4bf713ecb57c49c9f4594569ad63f3b6b58b8370
Azuzi <azuzii@proton.me>
2025-02-07 10:30:40 +0100

    feat: implement ProductService with create, find, and findOne methods

---
+++ src/modules/product/product.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { FindManyOptions, FindOneOptions } from 'typeorm';
+import { CreateProductInput } from './dto/product-input.dto';
+import { ProductOutput } from './dto/product-output.dto';
+import { Product } from './entities/product.entity';
+import { IProduct } from './interfaces/product.interface';
+import { productRepository } from './repositories/product.repository';
+
+@Injectable()
+export class ProductService implements IProduct {
+  constructor(private readonly productRepository: productRepository) {}
+
+  async createProduct(product: CreateProductInput) {
+    const savedProduct = await this.productRepository.save(product);
+    return plainToInstance(ProductOutput, savedProduct, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  find(options?: FindManyOptions<Product>): Promise<Product[]> {
+    return this.productRepository.find(options);
+  }
+
+  findOne(options: FindOneOptions<Product>): Promise<Product | null> {
+    return this.productRepository.findOne(options);
+  }
+}
