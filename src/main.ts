214fe91c846316caa64c69417be58ab4a45496a1
Azuzi <azuzii@proton.me>
2025-02-07 09:28:54 +0100

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1/');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  });
   await app.listen(3000);
 }
 bootstrap();
