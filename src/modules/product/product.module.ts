5cd054fb1811859cf1162fb26a3be69e83121a7d
Azuzi <azuzii@proton.me>
2025-02-07 10:33:29 +0100

    feat: create ProductModule, register Product entity, provide ProductService, productRepository, and IProduct

---
+++ src/modules/product/product.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { IProduct } from './interfaces/product.interface';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
+import { productRepository } from './repositories/product.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Product])],
+  controllers: [ProductController],
+  providers: [
+    ProductService,
+    productRepository,
+    {
+      provide: IProduct,
+      useExisting: ProductService,
+    },
+  ],
+})
+export class ProductModule {}
