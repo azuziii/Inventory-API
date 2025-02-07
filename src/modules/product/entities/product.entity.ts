8542dd97428cc88d3241cd62d0a296ca5b293939
Azuzi <azuzii@proton.me>
2025-02-07 09:42:50 +0100

    feat: Create Product entity

---
+++ src/modules/product/entities/product.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('product')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'text', nullable: false, unique: true })
+  name!: string;
+
+  @Column({ type: 'float', nullable: false, default: '' })
+  price!: string;
+}
