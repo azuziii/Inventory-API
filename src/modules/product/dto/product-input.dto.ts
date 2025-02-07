9f5f605bdc2b048f8b5e450cc60f8820e3fa5bbf
Azuzi <azuzii@proton.me>
2025-02-07 09:43:09 +0100

    feat: create product input dto

---
+++ src/modules/product/dto/product-input.dto.ts
@@ -0,0 +1,15 @@
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateProductInput {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Transform(({ value }) => {
+    return String(value);
+  })
+  price: string;
+}
