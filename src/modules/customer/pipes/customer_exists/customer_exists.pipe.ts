3dd34c92cae4fad755e873c80659119f671f5240
Azuzi <azuzii@proton.me>
2025-02-08 09:26:52 +0100

    fix: check if body has a name property before chekcing if name is taken, and rename service from userService to CustomerService

--- src/modules/customer/pipes/customer_exists/customer_exists.pipe.ts
+++ src/modules/customer/pipes/customer_exists/customer_exists.pipe.ts
@@ -10,19 +10,19 @@ import { ICustomer } from '../../interfaces/customer.interface';
 
 @Injectable()
 export class CustomerExistsPipe implements PipeTransform {
-  constructor(@Inject(ICustomer) private readonly userService: ICustomer) {}
+  constructor(@Inject(ICustomer) private readonly customerService: ICustomer) {}
 
   async transform(customer: CreateCustomerInput, metadata: ArgumentMetadata) {
-    const isCustomerExist = await this.userService.findOne({
+    if (!customer.name) return customer;
+
+    const isCustomerExist = await this.customerService.findOne({
       where: { name: customer.name },
     });
 
     if (isCustomerExist) {
       throw new BadRequestException(
-        `Customer with name ${customer.name} already exists`,
+        `Customer with name '${customer.name}' already exists`,
       );
     }
-
-    return customer;
   }
 }
