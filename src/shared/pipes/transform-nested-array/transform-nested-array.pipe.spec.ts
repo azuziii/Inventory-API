015848b29fdfe6c554d8a76a5a52dd790ccaa5f6
Azuzi <azuzii@proton.me>
2025-02-08 17:07:31 +0100

    feat: add TransformNestedArrayPipe

---
+++ src/shared/pipes/transform-nested-array/transform-nested-array.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { TransformNestedArrayPipe } from './transform-nested-array.pipe';
+
+describe('TransformNestedArrayPipe', () => {
+  it('should be defined', () => {
+    expect(new TransformNestedArrayPipe()).toBeDefined();
+  });
+});
