ef45ed82c67745793d3561746dbba857fc498d9f
Azuzi <azuzii@proton.me>
2025-02-14 15:20:56 +0100

    feat: add editOrderItem

--- src/modules/order/controllers/order-item.controller.ts
+++ src/modules/order/controllers/order-item.controller.ts
@@ -4,12 +4,16 @@ import {
   Delete,
   Param,
   ParseUUIDPipe,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { GetProductByIdPipe } from 'src/modules/product/pipes/get-product-by-id/get-product-by-id.pipe';
 import { ApiResponse } from 'src/shared/dto/api-response.dto';
-import { CreateOrderItemInput } from '../dto/order-item-input.dto';
+import {
+  CreateOrderItemInput,
+  UpdateOrderItem,
+} from '../dto/order-item-input.dto';
 import { OrderItemOutput } from '../dto/order-item-output.dto';
 import { GetOrderByIdPipe } from '../pipes/get-order-by-id/get-order-by-id.pipe';
 import { OrderItemService } from '../services/order-item.service';
@@ -24,6 +28,20 @@ export class OrderItemController {
     input: CreateOrderItemInput,
   ): Promise<ApiResponse<OrderItemOutput>> {
     const item = await this.orderItemService.createOrderItem(input);
+    return {
+      data: plainToInstance(OrderItemOutput, item, {
+        excludeExtraneousValues: true,
+      }),
+      meta: {},
+    };
+  }
+
+  @Patch()
+  async editOrderItem(
+    @Body(GetProductByIdPipe)
+    input: UpdateOrderItem,
+  ): Promise<ApiResponse<OrderItemOutput>> {
+    const item = await this.orderItemService.editOrderItem(input);
     return {
       data: plainToInstance(OrderItemOutput, item, {
         excludeExtraneousValues: true,
