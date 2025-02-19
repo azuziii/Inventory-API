554b458fbf75865228ef9185c1e2da181d597c06
Azuzi <azuzii@proton.me>
2025-02-19 16:20:41 +0100

    feat: create shipment item input dto

---
+++ src/modules/shipment/dto/shipment-item-input.ts
@@ -0,0 +1,38 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { Transform } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+import { Product } from 'src/modules/product/entities/product.entity';
+import { Shipment } from '../entities/shipment.entity';
+
+export class CreateShipmentItemInput {
+  @IsNotEmpty()
+  product!: Product;
+
+  @IsOptional()
+  @IsNumber()
+  @Transform(({ value }) => {
+    return Number(value);
+  })
+  quantity?: number;
+
+  @IsOptional()
+  @IsString()
+  cdn?: string;
+
+  @IsNotEmpty()
+  shipment!: Shipment;
+}
+
+export class UpdateShipmentitemInput extends PartialType(
+  CreateShipmentItemInput,
+) {
+  @IsOptional()
+  @IsUUID()
+  id?: string;
+}
