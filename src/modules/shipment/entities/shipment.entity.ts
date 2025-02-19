21024b4d6e58c89a8d8907d0553e7a243d5214ad
Azuzi <azuzii@proton.me>
2025-02-19 16:20:10 +0100

    feat: create shipment entity

---
+++ src/modules/shipment/entities/shipment.entity.ts
@@ -0,0 +1,39 @@
+import { Customer } from 'src/modules/customer/entities/customer.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { carEnum, DestinationEnum } from '../enum/shipment.enum';
+import { ShipmentItem } from './shipment-item.entity';
+
+@Entity('sipment')
+export class Shipment {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'timestamptz', nullable: false, default: () => 'NOW()' })
+  date!: Date;
+
+  @Column({ nullable: false, default: 0 })
+  bon!: number;
+
+  @Column({ default: 1 })
+  travels!: number;
+
+  @Column({ enum: carEnum })
+  type!: carEnum;
+
+  @Column({ enum: DestinationEnum, default: DestinationEnum.ALLER })
+  destination!: DestinationEnum;
+
+  @ManyToOne(() => Customer, { eager: true })
+  @JoinColumn()
+  customer!: Customer;
+
+  @OneToMany(() => ShipmentItem, (item) => item.shipment)
+  shipments!: ShipmentItem[];
+}
