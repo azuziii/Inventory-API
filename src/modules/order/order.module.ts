47473ff9ab8780abc2ef8579ced689b39f9c5ec3
Azuzi <azuzii@proton.me>
2025-02-27 16:51:32 +0000

    feat: export IOrder and IOrderItem

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -4,34 +4,32 @@ import { CustomerModule } from '../customer/customer.module';
 import { ProductModule } from '../product/product.module';
 import { OrderItemController } from './controllers/order-item.controller';
 import { OrderController } from './controllers/order.controller';
+import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 import { IOrderItem } from './interfaces/order-item.interface';
 import { IOrder } from './interfaces/order.interface';
-import { OrderItemRepository } from './repositories/order-item.repository';
-import { OrderRepository } from './repositories/order.repository';
 import { OrderItemService } from './services/order-item.service';
 import { OrderService } from './services/order.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order, OrderItemRepository]),
+    TypeOrmModule.forFeature([Order, OrderItem]),
     CustomerModule,
     ProductModule,
   ],
   controllers: [OrderController, OrderItemController],
   providers: [
     OrderService,
-    OrderRepository,
     {
       provide: IOrder,
       useExisting: OrderService,
     },
     OrderItemService,
-    OrderItemRepository,
     {
       provide: IOrderItem,
       useExisting: OrderItemService,
     },
   ],
+  exports: [IOrder, IOrderItem],
 })
 export class OrderModule {}
