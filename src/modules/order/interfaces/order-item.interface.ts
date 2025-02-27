e71e4dd91cc6929d129a0849e63f2c6ab541be38
Azuzi <azuzii@proton.me>
2025-02-27 16:50:34 +0000

    feat: add updateQuantity, find, findOne, and createOrderItemBulk

--- src/modules/order/interfaces/order-item.interface.ts
+++ src/modules/order/interfaces/order-item.interface.ts
@@ -1,10 +1,20 @@
-import { CreateOrderItemInput } from '../dto/order-item-input.dto';
+import { FindManyOptions, FindOneOptions } from 'typeorm';
+import {
+  CreateOrderItemInput,
+  CreateOrderItemInputBulk,
+} from '../dto/order-item-input.dto';
 import { OrderItem } from '../entities/order-item.entity';
 
 export const IOrderItem = 'IOrderItem';
 
 export interface IOrderItem {
-  // find(options?: FindManyOptions<OrderItem>): Promise<OrderItem[]>;
-  // findOne(options: FindOneOptions<OrderItem>): Promise<OrderItem | null>;
+  find(options?: FindManyOptions<OrderItem>): Promise<OrderItem[]>;
+  findOne(options: FindOneOptions<OrderItem>): Promise<OrderItem | null>;
   createOrderItem(input: CreateOrderItemInput): Promise<OrderItem>;
+  updateQuantity(
+    cdn: string,
+    productId: string,
+    quantity: number,
+  ): Promise<OrderItem>;
+  createOrderItemBulk(input: CreateOrderItemInputBulk[]): Promise<void>;
 }
