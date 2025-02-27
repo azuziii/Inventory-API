de180caf262319d71401296891979042f142c01f
Azuzi <azuzii@proton.me>
2025-02-27 16:50:05 +0000

    feat: add orders property to CreateOrderInput

--- src/modules/order/dto/order-input.dto.ts
+++ src/modules/order/dto/order-input.dto.ts
@@ -8,6 +8,10 @@ import {
   IsUUID,
 } from 'class-validator';
 import { Customer } from 'src/modules/customer/entities/customer.entity';
+import {
+  CreateOrderItemInput,
+  CreateOrderItemInputBulk,
+} from './order-item-input.dto';
 
 export class CreateOrderInput {
   @IsNotEmpty()
@@ -21,10 +25,29 @@ export class CreateOrderInput {
 
   @IsNotEmpty()
   customer!: Customer;
+
+  @IsOptional()
+  orders?: CreateOrderItemInput[];
 }
 
 export class UpdateOrderInput extends PartialType(CreateOrderInput) {
   @IsOptional()
   @IsUUID()
   id?: string;
+}
+export class CreateOrderInputBulk {
+  @IsNotEmpty()
+  @Transform(({ value }) => new Date(value))
+  @IsDate()
+  date!: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  cdn!: string;
+
+  @IsNotEmpty()
+  customer!: string;
+
+  @IsOptional()
+  orders?: CreateOrderItemInputBulk[];
 }
