eba43e4a2708620df51e7e12f504e6e8a8113438
Azuzi <azuzii@proton.me>
2025-02-27 16:56:51 +0000

    feat: add items property to CreateShipmentInput, and create GetCdnInput

--- src/modules/shipment/dto/shipment-input.ts
+++ src/modules/shipment/dto/shipment-input.ts
@@ -6,10 +6,15 @@ import {
   IsNotEmpty,
   IsNumber,
   IsOptional,
+  IsString,
   IsUUID,
 } from 'class-validator';
 import { Customer } from 'src/modules/customer/entities/customer.entity';
 import { carEnum, DestinationEnum } from '../enum/shipment.enum';
+import {
+  CreateShipmentItemInput,
+  CreateShipmentItemInputBulk,
+} from './shipment-item-input';
 
 export class CreateShipmentInput {
   @IsNotEmpty()
@@ -35,10 +40,61 @@ export class CreateShipmentInput {
 
   @IsNotEmpty()
   customer!: Customer;
+
+  @IsOptional()
+  items!: CreateShipmentItemInput[];
 }
 
 export class UpdateShipmentInput extends PartialType(CreateShipmentInput) {
   @IsOptional()
   @IsUUID()
   id?: string;
+}
+
+export class GetCdnInput {
+  @IsNotEmpty()
+  @IsString()
+  product!: string;
+
+  @IsNumber()
+  @Transform(({ value }) => {
+    return Number(value);
+  })
+  quantity!: number;
+
+  @IsNotEmpty()
+  @IsString()
+  customer!: string;
+
+  @IsOptional()
+  items!: CreateShipmentInput[];
+}
+
+export class CreateShipmentInputBulk {
+  @IsNotEmpty()
+  @Transform(({ value }) => new Date(value))
+  @IsDate()
+  date!: Date;
+
+  @IsNotEmpty()
+  @IsNumber()
+  bon!: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  travels!: number;
+
+  @IsNotEmpty()
+  @IsEnum(carEnum)
+  type!: carEnum;
+
+  @IsNotEmpty()
+  @IsEnum(DestinationEnum)
+  destination!: DestinationEnum;
+
+  @IsNotEmpty()
+  customer!: string;
+
+  @IsOptional()
+  items!: CreateShipmentItemInputBulk[];
 }
