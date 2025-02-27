23f1cd14c69e078eebdf50efbd059745581fab57
Azuzi <azuzii@proton.me>
2025-02-27 16:51:17 +0000

    feat: add findOne, createOrderItemBulk, find, and updateQuantity methods

--- src/modules/order/services/order-item.service.ts
+++ src/modules/order/services/order-item.service.ts
@@ -1,13 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IProduct } from 'src/modules/product/interfaces/product.interface';
+import { FindManyOptions, FindOneOptions, Repository } from 'typeorm';
 import { UpdateOrderInput } from '../dto/order-input.dto';
 import { CreateOrderItemInput } from '../dto/order-item-input.dto';
 import { OrderItem } from '../entities/order-item.entity';
 import { IOrderItem } from '../interfaces/order-item.interface';
-import { OrderItemRepository } from '../repositories/order-item.repository';
+import { IOrder } from '../interfaces/order.interface';
 
 @Injectable()
 export class OrderItemService implements IOrderItem {
-  constructor(private readonly orderItemRepository: OrderItemRepository) {}
+  constructor(
+    @InjectRepository(OrderItem)
+    private readonly orderItemRepository: Repository<OrderItem>,
+    @Inject(IProduct) private readonly productService: IProduct,
+    @Inject(forwardRef(() => IOrder)) private readonly orderService: IOrder,
+  ) {}
+
+  findOne(options: FindOneOptions<OrderItem>): Promise<OrderItem | null> {
+    return this.orderItemRepository.findOne(options);
+  }
+
+  async createOrderItemBulk(input: any[]): Promise<void> {
+    for (let item of input) {
+      const product = await this.productService.findOne({
+        where: { name: item.product },
+      });
+
+      const order = await this.orderService.findOne({
+        where: { id: item.iid },
+      });
+
+      await this.orderItemRepository.save({
+        ...item,
+        product,
+        order,
+        // quantity_left: item.quantity,
+      } as CreateOrderItemInput);
+    }
+  }
+
+  async find(options?: FindManyOptions<OrderItem>): Promise<OrderItem[]> {
+    const f = await this.orderItemRepository.find(options);
+    await Promise.all(
+      f
+        .map((x) => ({ ...x, quantity_left: x.quantity }))
+        .map((x) => this.orderItemRepository.save(x)),
+    );
+    return this.orderItemRepository.find(options);
+  }
 
   createOrderItem(input: CreateOrderItemInput): Promise<OrderItem> {
     return this.orderItemRepository.save(input);
@@ -15,6 +56,38 @@ export class OrderItemService implements IOrderItem {
 
   editOrderItem(input: UpdateOrderInput): Promise<OrderItem> {
     return this.orderItemRepository.save(input);
+  }
+
+  async updateQuantity(
+    cdn: string,
+    productId: string,
+    quantity: number,
+  ): Promise<any> {
+    console.log(cdn, productId, quantity);
+    return this.orderItemRepository.manager.transaction(
+      async (transactionalEntityManager) => {
+        const orderItem = await transactionalEntityManager.findOne(OrderItem, {
+          where: {
+            product: {
+              name: productId,
+            },
+            order: {
+              cdn,
+            },
+          },
+          lock: { mode: 'pessimistic_write' },
+        });
+
+        if (!orderItem) {
+          return;
+        }
+
+        await transactionalEntityManager.save(OrderItem, {
+          ...orderItem,
+          quantity_left: orderItem.quantity_left + quantity,
+        });
+      },
+    );
   }
 
   async deleteOrder(id: string): Promise<void> {
